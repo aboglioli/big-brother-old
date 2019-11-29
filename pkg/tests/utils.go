a449213c76b476bcf83d6e767c16d77d735b9b4c
Alan Boglioli <alan.boglioli@gmail.com>
2019-11-29 19:44:32 -0300

    Add stack info to custom utils for testing

--- pkg/tests/utils.go
+++ pkg/tests/utils.go
@@ -1,51 +1,58 @@
 package tests
 
 import (
+	"fmt"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/aboglioli/big-brother/pkg/errors"
 )
 
+func printStackInfo() string {
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		panic("Couldn't get the caller information")
+	}
+
+	fn := runtime.FuncForPC(pc).Name()
+	parts := strings.Split(fn, "/")
+	fn = parts[len(parts)-1]
+
+	parts = strings.Split(file, "/")
+	file = parts[len(parts)-1]
+
+	return fmt.Sprintf("in %s (%s, line %d)", fn, file, line)
+}
+
 // Global
-func Assert(t *testing.T, cond bool, msg string) {
+func Assert(t *testing.T, cond bool, msgs ...string) {
 	if !cond {
-		t.Errorf("ASSERT: %s\n", msg)
+		t.Errorf("ASSERT: %s\n%s\n", msgs, printStackInfo())
 	}
 }
 
-func Equals(t *testing.T, v1 interface{}, v2 interface{}, msg string) {
+func Equals(t *testing.T, v1 interface{}, v2 interface{}, msgs ...string) {
 	if v1 != v2 {
-		t.Errorf("EQUALS: %s\n%v != %v\n", msg, v1, v2)
-	}
-}
-
-func IsNil(t *testing.T, v interface{}, msg string) {
-	if v != nil {
-		t.Errorf("IS NOT NIL: %s\n", msg)
-	}
-}
-
-func IsNotNil(t *testing.T, v interface{}, msg string) {
-	if v == nil {
-		t.Errorf("IS NIL: %s\n", msg)
+		t.Errorf("EQUALS: %s\n%v != %v\n%s\n", msgs, v1, v2, printStackInfo())
 	}
 }
 
 // Errors
-func Ok(t *testing.T, err error, msg string) {
+func Ok(t *testing.T, err error, msgs ...string) {
 	if err != nil {
-		t.Errorf("ERROR %s NOT EXPECTED: %s\n", err.Error(), msg)
+		t.Errorf("OK: %s\nError %s not expected\n%s\n", msgs, err.Error(), printStackInfo())
 	}
 }
 
-func Err(t *testing.T, err error, msg string) {
+func Err(t *testing.T, err error, msgs ...string) {
 	if err == nil {
-		t.Errorf("ERROR EXPECTED: %s\n", msg)
+		t.Errorf("ERR: %s\nError expected\n%s\n", msgs, printStackInfo())
 	}
 }
 
-func ErrCode(t *testing.T, err errors.Error, code string, msg string) {
+func ErrCode(t *testing.T, err errors.Error, code string, msgs ...string) {
 	if err == nil || err.Code() != code {
-		t.Errorf("CODE %s EXPECTED: %s\n", code, msg)
+		t.Errorf("ERR_CODE: %s\nError with code %s expected\n%s\n", msgs, code, printStackInfo())
 	}
 }
